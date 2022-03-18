5f5a42b88a64e5f222b887ac669624ce5d866692
victorramide <victorramide@gmail.com>
2022-03-17 23:03:03 -0300

    criação do app catalogo e do Model

---
+++ catalogo/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.forms import CharField
+
+# Create your models here.
+
+
+class Livro(models.Model):
+    titulo = models.CharField(max_length=50)
+    autor = models.CharField(max_length=30)
+    isbn = models.CharField(max_length=10)
+    quantidade_de_paginas = models.DecimalField(
+        max_digits=None, decimal_places=None)
+    capa = models.URLField()
+    editora = models.CharField(max_length=30)
+    edicao = models.DecimalField()
+    categoria = models.CharField(max_length=30)
+    sinopse = models.TextField()
+    data_de_publicacao = models.DateField()
+
+    def __str__(self):
+        return self.titulo
