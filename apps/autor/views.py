a873912695ce3cbed55148f3ad37d5320e657061
victorramide <victorramide@gmail.com>
2022-03-22 21:32:42 -0300

    Inclui módulo básico de segurança

--- apps/autor/views.py
+++ apps/autor/views.py
@@ -1,9 +1,13 @@
 from rest_framework import viewsets
 from apps.autor.models import Autor
 from apps.autor.serializer import AutorSerializer
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 
 class AutorViewSet(viewsets.ModelViewSet):
     """Lista com todos os autores"""
     queryset = Autor.objects.all()
     serializer_class = AutorSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
--- apps/catalogo/views.py
+++ apps/catalogo/views.py
@@ -1,9 +1,13 @@
 from rest_framework import viewsets
 from apps.catalogo.models import Livro
 from apps.catalogo.serializer import LivroSerializer
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 
 class LivrosViewSet(viewsets.ModelViewSet):
     """Lista todos os livros do catalogo"""
     queryset = Livro.objects.all()
     serializer_class = LivroSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
